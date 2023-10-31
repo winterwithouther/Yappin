9e45264fdbea57b39e483d5717127758c7e4cc5e
Eman Gurung <egurung1117@gmail.com>
2023-10-31 14:11:28 -0400

    added relationship to Post Model

--- server/models.py
+++ server/models.py
@@ -9,6 +9,11 @@ class Post(db.Model, SerializerMixin):
     content = db.Column(db.String)
     caption = db.Column(db.String)
 
+    comments = db.relationship("Comment", backref="post", cascade="all, delete-orphan")
+    likes = db.relationship("Like", backref="post", cascade="all, delete-orphan")
+
+    serialize_rules = ("-comments.post", "-likes.post",)
+
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -22,4 +27,4 @@ class Comment(db.Model, SerializerMixin):
 class Like(db.Model, SerializerMixin):
     __tablename__ = "likes"
     id = db.Column(db.Integer, primary_key=True)
-    
+
